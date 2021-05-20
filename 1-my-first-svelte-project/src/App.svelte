f1b67e0b4ed794d8c6b1f4b0f3978697c2dde04c
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-05-20 14:43:11 +0200

    Update App.svelte

--- 1-my-first-svelte-project/src/App.svelte
+++ 1-my-first-svelte-project/src/App.svelte
@@ -1,12 +1,18 @@
 <script>
-	let todos = ['Learn Svelte', 'Make something cool', 'Pause', 'Weiterarbeiten'];
+	let todos = [
+		{ text: 'Learn Svelte', done: false },
+		{ text: 'Make something cool', done: false },
+		{ text: 'Carry on', done: false },
+	];
 
 	let inputFocussed = false;
 	let inputText = '';
 
+	let hoveredItem;
+
 	function handleNewItemClick() {
 		if (inputText != '') {
-			todos.push(inputText);
+			todos.push({ text: inputText, done: false });
 			todos = todos;
 			inputText = '';
 		}
@@ -33,16 +39,21 @@
 		<h2>Todos</h2>
 
 		{#each todos as todo, index}
-			<div class="item">
-				{todo}
-				<span class="remove-button" on:click={() => removeItem(index)}>Remove</span>
+			<div class="item" on:mouseenter={() => (hoveredItem = index)} on:mouseleave={() => (hoveredItem = undefined)}>
+				<div class="item-text" class:done={todo.done} on:click={() => todo.done = !todo.done}>{todo.text}</div>
+
+				{#if hoveredItem == index}
+					<div class="remove-button" on:click={() => removeItem(index)}>Remove</div>
+				{/if}
+
+				<!-- <span class="remove-button" on:click={() => removeItem(index)} style={hoveredItem == index ? "" : "display:none"}>Remove2</span> -->
 			</div>
 		{/each}
 
 		<input type="text" bind:value={inputText} on:focus={handleInputFocus} on:blur={handleInputBlur} />
 
 		{#if inputText != '' || inputFocussed}
-			<button on:click={handleNewItemClick}>Add new item</button>
+			<button on:click={handleNewItemClick}>Add</button>
 		{/if}
 	</div>
 </main>
@@ -52,22 +63,33 @@
 		position: absolute;
 		left: 200px;
 		top: 100px;
+		width: 300px;
 		border: 1px solid black;
 		padding: 10px;
 		border-radius: 5px;
 	}
 
+	/* Container */
 	.item {
+		display: flex;
+		justify-content: space-between;
+		align-items: baseline;
 		padding: 10px 0px;
+		user-select: none;
 	}
 
-	input {
-		margin-top: 10px;
+	.item-text.done {
+		text-decoration: line-through;
 	}
 
 	.remove-button {
 		font-size: small;
 		color: red;
-		user-select: none;
+		/* user-select: none; */
+		/* float:right; */
+	}
+
+	input {
+		margin-top: 10px;
 	}
 </style>
