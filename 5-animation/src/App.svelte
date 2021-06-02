1c053fe7a68ca35c0cec08ba2dd0a5de49c03f1b
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-06-02 08:09:11 +0200

    added animation examples

---
+++ 5-animation/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	let x = 100;
+	let y = 100;
+	
+	function handleClick() {
+		 x = Math.random() * (window.innerWidth - 100);
+		 y = Math.random() * (window.innerHeight - 100);
+	}
+
+</script>
+
+
+<div id="box" on:click={handleClick} style="left: {x}px; top: {y}px;">
+
+</div>
+
+
+<style>
+	#box {
+		position: absolute;
+		width: 100px;
+		height: 100px;
+		background-color: orchid;
+		transition: all 1s ease-in-out;
+	}
+
+</style>
---
+++ 6-fly-in-out/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { fly } from 'svelte/transition';
+	let items = [
+		{ id: 1, text: 'Oranges' },
+		{ id: 2, text: 'Apples' },
+		{ id: 3, text: 'Bananas' },
+		{ id: 4, text: 'Pears' },
+	];
+
+	function handleItemClick(i) {
+		console.log(i);
+		items.splice(i, 1);
+		items = items;
+	}
+</script>
+
+{#each items as item, index (item.id)}
+	<div class="item" on:click={() => handleItemClick(index)} transition:fly={{ x: 50, duration: 2000 }}>{item.text}</div>
+{/each}
+
+<style>
+	.item {
+		padding: 5px;
+		user-select: none;
+	}
+</style>
